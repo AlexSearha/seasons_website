e27a0ced91bec4d6a3b6a2c6ae0d25231ba4fbd0
AlexSearha <contact@mytechcompanion.fr>
2025-05-20 21:59:44 +0200

    majout de la lib particules

--- app/Layout/Header.tsx
+++ app/Layout/Header.tsx
@@ -5,6 +5,7 @@ import useSizeDetection from '@/hooks/sizeDetect';
 import MenuDesktop from './MenuDesktop';
 import MenuMobile from './MenuMobile';
 import ParagrapheSpace from '../Components/ParagrapheSpace';
+import ParticulesGeneric from '../Components/ParticulesGenerci';
 
 const Header = () => {
   const isMobileWindowsSize = useSizeDetection({});
@@ -17,6 +18,7 @@ const Header = () => {
         id="bg-overlay"
         className="absolute top-0 left-0 w-full h-full  bg-gradient-to-b from-black from-5% via-transparent via-85% z-10"
       ></div>
+      <ParticulesGeneric />
       <div className="relative flex flex-col w-full max-w-7xl z-20 mb-15">
         {isMobileWindowsSize ? <MenuMobile /> : <MenuDesktop />}
         <div className="h-title mt-0 md:mt-25 bg-[url(/pattern_old_paper.webp)] bg-clip-text text-transparent bg-cover">
